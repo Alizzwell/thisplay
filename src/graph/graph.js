aef973dab04a6117666fa1ab388c72ad93b1dabd
hehe717 <nawonjae@naver.com>
2016-10-13 10:59:32 +0900

    #1 Graph 1.makeUndirectedEdge 추가 2. deleteNode, deleteEdge 추가 3. makeNode (노드 id값 갖도록 수정) 4. force 값 수정

--- src/graph/graph.js
+++ src/graph/graph.js
@@ -18,26 +18,26 @@
 
     var width = 700;
     var height = 1000;
-    var radius = 30;
+    var radius = 50;
 
     var nodes = [];
 	  var links = [];
 
     var forceManyBody = d3.forceManyBody()
-        .strength(-2400)
+        .strength(-3500)
       //  .theta(0.9) // need to test
         .distanceMin(100) // ntt
-        .distanceMax(300); // ntt
+        .distanceMax(1000); // ntt
 
     var forceLink = d3.forceLink();
     forceLink
       .links(links)
       .distance(300) // ntt
-      .strength(0.7) // ntt
+      .strength(-200) // ntt
       .iterations(0.5); // ntt
 
     var forceCollide = d3.forceCollide()
-    .radius(2)
+    .radius(width/2)
     .strength(0.7)
     .iterations(1);
 
@@ -52,9 +52,8 @@
       .force("x", d3.forceX().strength(0.05))
       .force("y", d3.forceY().strength(0.1))
       .alphaMin(0.2)
-      .alphaDecay(0.03)
+      .alphaDecay(0.05)
       .velocityDecay(0.85)
-
       .on("tick", function () {
         svgNode.selectAll(".node")
           .attr("transform", function(d) { return 'translate(' + [d.x , d.y] + ')' ;})
@@ -63,12 +62,13 @@
 
         svgNode.selectAll(".nodetext")
           .attr("transform", function(d) {
-            return 'translate(' + [d.x, d.y+7] + ')' ;
+            return 'translate(' + [d.x, d.y+10] + ')' ;
           });
 
         svgLink.selectAll(".link")
           .attr("d", function(d) {
-             return drawLine(d);
+            if(d.is_directed === true) return drawDirectedLine(d);
+            else return drawUnDirectedLine(d);
         });
 
 				svgLink.selectAll(".linktext")
@@ -78,10 +78,16 @@
       });
 
 
+    
+    var drawUnDirectedLine = function (d) {
+      var sx = d.source.getAttribute("x"), sy = d.source.getAttribute("y");
+      var tx = d.target.getAttribute("x"), ty = d.target.getAttribute("y");
+      return "M" + sx + "," + sy + 
+      "L" + tx + "," + ty ;
+    }
 
 
-
-    var drawLine = function (d) {
+    var drawDirectedLine = function (d) {
       var sx = d.source.getAttribute("x"), sy = d.source.getAttribute("y");
       var tx = d.target.getAttribute("x"), ty = d.target.getAttribute("y");
       var dx = tx - sx;
@@ -89,14 +95,13 @@
       dx = dx * 3;
 			dy = dy * 3;
       var dr = Math.sqrt(dx * dx + dy * dy);
-      var theta = Math.atan2(dy, dx) + Math.PI / 26.55;
+      var theta = Math.atan2(dy, dx) + 0.2;
 			var d90 = Math.PI / 2;
-      var dtxs = tx - 1.22 * radius * Math.cos(theta);
-      var dtys = ty - 1.22 * radius * Math.sin(theta);
-      var val1 = 3.5, val2 = 10.5;
-
+      var dtxs = tx - 1.43 * radius * Math.cos(theta);
+      var dtys = ty - 1.43 * radius * Math.sin(theta);
+      var val1 = 3.5, val2 = 8.5;
       return "M" + sx + "," + sy +
-	      "A" + dr + "," + dr + " 0 0 1," + tx + "," + ty +
+	      "A" + dr + "," + dr + " 0 0 1," + dtxs + "," + dtys +
 	      "A" + dr + "," + dr + " 0 0 0," + sx + "," + sy +
 	      "M" + dtxs + "," + dtys +
 	      "l" + (val1 * Math.cos(d90 - theta) - val2 * Math.cos(theta)) + "," +
@@ -106,19 +111,16 @@
     };
 
     var drawText = function(d){
-        var sx = d.source.getAttribute("x"), sy = d.source.getAttribute("y");
-        var tx = d.target.getAttribute("x"), ty = d.target.getAttribute("y");
-				var dx = tx - sx;
-				var dy = ty - sy;
-				dx = dx * 3;
-			 	dy = dy * 3;
+      var sx = d.source.getAttribute("x"), sy = d.source.getAttribute("y");
+      var tx = d.target.getAttribute("x"), ty = d.target.getAttribute("y");
+      var dx = tx - sx;
+      var dy = ty - sy;
+      var dr = Math.sqrt(dx * dx + dy * dy),
+        theta = Math.atan2(dy, dx) + 0.3,
+        dtxs = tx - 5 * radius * Math.cos(theta),
+        dtys = ty - 5 * radius * Math.sin(theta);
 
-				var dr = Math.sqrt(dx * dx + dy * dy),
-					theta = Math.atan2(dy, dx) + Math.PI / 11.95,
-					d90 = Math.PI / 2,
-					dtxs = tx - 4 * radius * Math.cos(theta),
-					dtys = ty - 4 * radius * Math.sin(theta);
-				return 'translate(' + [dtxs, dtys] + ')';
+        return 'translate(' + [dtxs, dtys] + ')';
     };
 
     this.target = target;
@@ -132,19 +134,20 @@
     this.forceManyBody = forceManyBody;
     this.force = force;
     this.nodes = nodes;
-    this.drawLine = drawLine;
     this.links = links;
   }
 
   Graph.prototype.redraw = function () {
-     var that = this;
+    var that = this;
+    
     // draw link tag
     this.svgLink.selectAll('.link')
       .data(this.links)
       .enter()
       .insert("path")
       .attr("class", "link")
-      .attr("id", function(d) { return d.id; });
+      .attr("id", function(d) { return d.id; })
+      .attr("is_directed", function(d) { return d.is_directed; });
 
     this.svgLink.selectAll('.linktext')
       .data(this.links)
@@ -171,11 +174,13 @@
       .enter()
       .insert('text')
       .attr("class", "nodetext")
+      .attr("id", function(d) { return "text" + d.id; })
       .text(function(d) { return d.text; });
 
 
     function dragstarted(d) {
-      if (!d3.event.active) that.force.alpha(1).restart();
+      if (!d3.event.active) that.force.alpha(0.6).restart();
+      that.force.alpha(0.3);
       d.fx = d.x;
       d.fy = d.y;
     }
@@ -186,20 +191,29 @@
     }
 
     function dragended(d) {
-      if (!d3.event.active) that.force.alphaTarget(0.15);
+      if (!d3.event.active) that.force.alphaTarget(0.2);
       d.fx = null;
       d.fy = null;
     }
   };
 
-  Graph.prototype.makeNode = function (id) {
-    if(this.svgNode.select("#node_"+id).node() === null) {
-      this.nodes.push({id: "node_" + id, text: id});
+  Graph.prototype.makeNode = function (idx, text) {
+    if(this.svgNode.select("#node_"+idx).node() === null) {
+      this.nodes.push({id: "node_" + idx, idx: idx, text: text});
+    }
+    else {
+      for(var i = 0; i < this.nodes.length; i++){
+        if(this.nodes[i].idx === idx){
+          this.nodes[i].text = text;
+          this.svgNode.select("#textnode_" + idx).text(text);
+          break;
+        }
+      }
     }
     this.force.nodes(this.nodes).alpha(1).restart();
     this.redraw();
   };
-
+  
   Graph.prototype.makeEdge = function (source, target, value) {
     var that = this;
     var snode, tnode;
@@ -216,7 +230,7 @@
         source : snode,
         target : tnode,
         value : value,
-        is_directed_array : false
+        is_directed: true
       });
     }
     else{
@@ -235,24 +249,119 @@
     this.force.alpha(1);
     this.force.restart();
     this.redraw();
-   };
+  };
+   
+  Graph.prototype.makeUnDirectedEdge = function (source, target, value) {
+    var that = this;
+    var snode, tnode;
+    var tmp;
+    
+    if(source > target){
+      tmp = source;
+      source = target;
+      target = tmp;
+    }
+    
+    var edge = this.svgLink.select("#link_" + source + "_" + target).node();
+    snode = this.svgNode.select("#node_" + source).node();
+    tnode = this.svgNode.select("#node_" + target).node();
 
-  Graph.prototype.highlightNode = function (id) {
-   	var node = this.svg.select("#node_" + id);
+    if(snode === null || tnode === null){
+      // pr("no node");
+    }
+    else if(edge === null){
+      this.links.push({
+        id : "link_" + source + "_" + target,
+        source : snode,
+        target : tnode,
+        value : value,
+        is_directed: false
+      });
+    }
+    else{
+      for(var i = 0; i < this.links.length; i++){
+        if(this.links[i].id === "link_" + source + "_" + target){
+          this.links[i].value = value;
+          this.svgLink.select("#textlink_" + source + "_" + target).text(value);
+          break;
+        }
+      }
+    }
+
+    this.forceLink
+      .links(this.links);
+
+    this.force.alpha(1).restart();
+    this.redraw();
+   };
+   
+  Graph.prototype.highlightNode = function (idx) {
+   	var node = this.svg.select("#node_" + idx);
     if(node.node() !== null) {
       node.transition().duration(500).style("fill", "red");
     }
   };
 
   Graph.prototype.highlightEdge = function (source, target) {
-   	var edge = this.svg.select("#link_" + source + "_" + target);
+   	var edge = this.svg.select("#link_" + source + "_" + target); 
+    var trg_to_src_edge = this.svg.select("#link_" + target + "_" + source);
+    if(trg_to_src_edge.node() != null){
+      if(trg_to_src_edge.node().getAttribute("is_directed") === false) {
+        edge = this.svg.select("#link_" + target + "_" + source);
+      }
+    }    
+    pr(edge);
     if(edge.node() !== null) {
       edge.transition().duration(500).style("stroke", "red");
     }
   };
-
-  Graph.prototype.unHighlightNode = function (id) {
-   	var node = this.svg.select("#node_" + id);
+  
+  Graph.prototype.deleteEdge = function (source, target){
+    var snode = this.svgNode.select("#node_" + source).node();
+    var tnode = this.svgNode.select("#node_" + target).node();
+    
+    if(source == target || snode == null || tnode == null) return ;
+    
+    for(var i = 0; i<this.links.length; i++){
+      if(this.links[i].source == snode && this.links[i].target == tnode){
+         this.links.splice(i, 1);
+      }
+ 
+      if(this.links[i].source == tnode && this.links[i].target == snode 
+        && this.links[i].is_directed === false){
+        pr("get");
+        this.links.splice(i, 1);
+        this.svgLink.select("#link_" + target + "_" + source).remove();
+        this.svgLink.select("#textlink_" + target + "_" + source).remove();
+      }
+    }    
+    
+    this.svgLink.select("#link_" + source + "_" + target).remove();
+    this.svgLink.select("#textlink_" + source + "_" + target).remove();
+    this.forceLink.links(this.links);
+    this.force.alpha(1).restart();
+    this.redraw();
+  }
+  
+  Graph.prototype.deleteNode = function (idx){
+    for(var i=0; i<this.nodes.length; i++){
+      if(this.nodes[i].idx === idx){
+        for(var j=0; j<this.nodes.length; j++){
+          this.deleteEdge(idx, j);
+          this.deleteEdge(j, idx);
+        }
+        this.svgNode.select("#"+this.nodes[i].id).remove();
+        this.svgNode.select("#text"+this.nodes[i].id).remove();
+        this.nodes.splice(i, 1);
+      }
+    }
+    
+    this.force.nodes(this.nodes).alpha(1).restart();
+    this.redraw();
+  }
+  
+  Graph.prototype.unHighlightNode = function (idx) {
+   	var node = this.svg.select("#node_" + idx);
     if(node.node() !== null) {
       node.transition().duration(500).style("fill", "black");
     }
@@ -264,10 +373,10 @@
       edge.transition().duration(500).style("stroke", "#999");
     }
   };
-
+  
   Graph.prototype.unHighlightNodeAll = function () {
     for(var i = 0; i < this.nodes.length; i++){
-      this.unHighlightNode(this.nodes[i].text);
+      this.unHighlightNode(this.nodes[i].idx);
     }
   };
 
@@ -287,7 +396,9 @@
     this.links = [];
     this.redraw();
   };
-
+  function pr(a) {
+    console.log(a);
+  }
   thisplay.Graph = Graph;
 
 })(thisplay, d3);
